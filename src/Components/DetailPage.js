fcd01cc1dea522c20af10fb635b85420af7af6c8
dev05 <dev05@dev.co-mit.com>
2025-04-29 11:20:15 +0700

    Les 4: Detail story page and info story

--- src/Components/DetailPage.js
+++ src/Components/DetailPage.js
@@ -1,12 +1,88 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Badge, Card, Col, Container, Row } from 'react-bootstrap';
+import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
 
 const DetailPage = () => {
   const {slug} = useParams();
+
+  const [getdata, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const item = getdata?.data?.data?.item;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://otruyenapi.com/v1/api/truyen-tranh/${slug}`);
+        setData(response);
+        setLoading(false);
+        console.log(response);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [slug]);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error}</p>
+
   return (
-    <div>
-      {slug}
-    </div>
+    <>
+      <Helmet>
+        <title>{getdata.data.data.seoOnPage.titleHead}</title>
+      </Helmet>
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Title>{getdata.data.data.seoOnPage.titleHead}</Card.Title>
+                {getdata.data.data.seoOnPage.descriptionHead}
+                </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card style={{width: "30rem"}}>
+              <Card.Img
+                variant="top" 
+                src={getdata.data.data.APP_DOMAIN_CDN_IMAGE + `/uploads/comics/` + item.thumb_url} 
+              />
+              <Card.Body>
+                <Card.Title>{item.name || "No Title"}</Card.Title>
+                <Card.Title
+                  dangerouslySetInnerHTML={{__html: item.content}}
+                ></Card.Title>
+                <Card.Text>{item.updatedAt}</Card.Text>
+                <Card.Text>{item.status}</Card.Text>
+                <Card.Text>
+                  {item.author && item.author.length > 0 ? item.author.map((author, index) => (
+                    <Badge bg="info" key={index}>
+                      {author.name}
+                    </Badge>
+                  ))
+                  : "Others"}
+                </Card.Text>
+                <Card.Text>
+                  {item.category && item.category.length > 0 ? item.category.map((category, index) => (
+                    <Badge bg="info" key={index}>
+                      {category.name}
+                    </Badge>
+                  ))
+                  : "Others"}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
